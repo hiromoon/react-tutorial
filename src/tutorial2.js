844a01da97f84696a9e813f92d1cc6a54ac90cf8
hiromoon <hiromoon0428@gmail.com>
2016-01-02 17:56:16 +0900

    react tutorian No.11

---
+++ src/tutorial2.js
@@ -0,0 +1,18 @@
+var CommentList = React.createClass({
+  render: function(){
+    return (
+        <div className="commentList">
+          Hello, world! I am a CommentList.
+        </div>
+        );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    <div className="commentForm">
+      Hello, world! I am a CommentFrom.
+    </div>
+   );
+  }
+});
